2aa97675a12386d464fc3d05d2046c0e358f28ab
shubhampachghare <shubhampachghare@gmail.com>
2023-07-02 18:11:27 +0530

    [Shubham] -Examples

---
+++ Polymorphism/Polymorphism.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Animal
+    {
+        public virtual void AnimalSound()                       //Base Class
+        {
+            Console.WriteLine("Animal Makes a Sound");
+        }
+    }
+    class Pig : Animal 
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Pig Says Wee Wee");
+        }
+    }
+
+    class Dog:Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("The Dog Says : Bow Bow");
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal mypig = new Pig();
+            Animal myDog= new Dog();
+
+            myAnimal.AnimalSound();
+            myDog.AnimalSound();
+            mypig.AnimalSound();
+
+
+        }
+    }
+}
